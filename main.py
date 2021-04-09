2a962a9d3ec3d72464ff2f2fffcaf40c109eb910
Plasma <56369290+PlasmaAsDev@users.noreply.github.com>
2021-04-09 02:19:10 -0300

    Bug fixes

--- main.py
+++ main.py
@@ -2,6 +2,7 @@
 import discord, aiohttp, datetime
 from discord.ext import commands
 from discord import Webhook, AsyncWebhookAdapter
+from discord.errors import InvalidArgument, HTTPException
 import json
 
 with open("config.json") as configuration_file:
@@ -30,115 +31,99 @@ async def on_ready():
     print("My body is ready")
     print(f"Logged in as: {bot.user}")
     
-@bot.command()
-async def ping(ctx):
-    await ctx.send("pong")
-
 @bot.event
 async def on_member_join(member):
-    if member.bot:
-        try:
-
-            for role_id in config['roles']['bot-roles']:
-                role = member.guild.get_role(role_id)
+    if member.bot: #If the member who join is a bot....
+        if str(type(config['roles']['bot-roles'])) == "<class 'int'>":
+            role = member.guild.get_role(config['roles']['user-roles'])
+            if role != None:
                 await member.add_roles(role, reason="BOT autorole")
                 if config['webhook']['enabled']:
-                    try:
-                        webhook_url = str(config['webhook']['url'])
-
-
-                        async with aiohttp.ClientSession() as session:
-
-                            webhook = Webhook.from_url(webhook_url , adapter=AsyncWebhookAdapter(session))
-
-                            embed = discord.Embed(title=f"{member} is a bot", description=f"Bot detected, role {role.mention} assigned", color=0x23cf4b,timestamp=datetime.datetime.utcnow())
-                            if str(config['webhook']['custom-image']).startswith("https") or str(config['webhook']['custom-image']).startswith("http"):
-
-                                await webhook.send(embed=embed, username=config['webhook']['custom-username'], avatar_url=config['webhook']['custom-image'])
-                            else:
-                                await webhook.send(embed=embed, username=config['webhook']['custom-username'])
-
-                    except Exception as e:
-                        print(e)
-    
-        except TypeError:
-            role = member.guild.get_role(config['roles']['bot-roles'])
-            await member.add_roles(role, reason="BOT autorole")
-            if config['webhook']['enabled']:
-                try:
                     webhook_url = str(config['webhook']['url'])
-
-
                     async with aiohttp.ClientSession() as session:
-
                         webhook = Webhook.from_url(webhook_url , adapter=AsyncWebhookAdapter(session))
 
-                        embed = discord.Embed(title=f"{member} is a bot", description=f"Bot detected, role {role.mention} assigned", color=0x23cf4b,timestamp=datetime.datetime.utcnow())
-                        if str(config['webhook']['custom-image']).startswith("https") or str(config['webhook']['custom-image']).startswith("http"):
-
-
+                        embed = discord.Embed(title=f"{member} is a bot", description=f"Bot detected, role {role.mention} assigned", color=0x23cf4b,timestamp=datetime.datetime.utcnow()) #You can edit this embed message
+                        try:
                             await webhook.send(embed=embed, username=config['webhook']['custom-username'], avatar_url=config['webhook']['custom-image'])
-                        else:
+                        except HTTPException:
+                            print("Specify a valid image url on the config file")
                             await webhook.send(embed=embed, username=config['webhook']['custom-username'])
 
-                except Exception as e:
-                    print(e)
+            elif role == None:
+                raise TypeError("You have to give me a valid id")
+    
+        elif str(type(config['roles']['bot-roles'])) == "<class 'list'>":
+            role = member.guild.get_role(config['roles']['bot-roles'])
+            if role != None:
 
-    elif not member.bot:
-        try:
+                await member.add_roles(role, reason="BOT autorole")
+                if config['webhook']['enabled']:
+                    webhook_url = str(config['webhook']['url'])
+                    async with aiohttp.ClientSession() as session:
+                        webhook = Webhook.from_url(webhook_url , adapter=AsyncWebhookAdapter(session))
 
+                        embed = discord.Embed(title=f"{member} is a bot", description=f"Bot detected, role {role.mention} assigned", color=0x23cf4b,timestamp=datetime.datetime.utcnow()) #You can edit this embed message
+                        try:
+                            await webhook.send(embed=embed, username=config['webhook']['custom-username'], avatar_url=config['webhook']['custom-image'])
+                        except HTTPException:
+                            print("Specify a valid image url on the config file")
+                            await webhook.send(embed=embed, username=config['webhook']['custom-username'])
 
-            for role_id in config['roles']['user-roles']:
-                role = member.guild.get_role(role_id)
+            elif role == None:
+                raise TypeError("You have to give me a valid id")
+    
+    elif not member.bot: #If the member who join is not a bot....
+    
+        if str(type(config['roles']['user-roles'])) == "<class 'int'>":
+            role = member.guild.get_role(config['roles']['user-roles'])
+            if role != None:
+
                 await member.add_roles(role, reason="Autorole")
                 if config['webhook']['enabled']:
-                    try:
+                    webhook_url = str(config['webhook']['url'])
+                    async with aiohttp.ClientSession() as session:
+                        try:
+                            webhook = Webhook.from_url(webhook_url , adapter=AsyncWebhookAdapter(session))
+                        except InvalidArgument:
+                            print("Specify a valid webhook url")
+
+                        embed = discord.Embed(title=f"{member} joined", description=f"Role {role.mention} assigned", color=0x23cf4b,timestamp=datetime.datetime.utcnow()) #You can edit this embed message
+
+                        #await webhook.send(embed=embed, username=config['webhook']['custom-username'], avatar_url=str(config['webhook']['custom-image']))
+                        try:
+                            await webhook.send(embed=embed, username=config['webhook']['custom-username'], avatar_url=config['webhook']['custom-image'])
+                        except HTTPException:
+                            print("Specify a valid image url on the config file")
+                            await webhook.send(embed=embed, username=config['webhook']['custom-username'])
+                        
+            elif role == None:
+                raise TypeError("You have to give me a valid id")
+        elif str(type(config['roles']['user-roles'])) == "<class 'list'>":
+            for role_id in config['roles']['user-roles']:
+                role = member.guild.get_role(role_id)
+                if role != None:
+                    await member.add_roles(role, reason="Autorole")
+                    if config['webhook']['enabled']:
+                    
                         webhook_url = str(config['webhook']['url'])
-
-
                         async with aiohttp.ClientSession() as session:
-
                             webhook = Webhook.from_url(webhook_url , adapter=AsyncWebhookAdapter(session))
 
-                            embed = discord.Embed(title=f"{member} joined", description=f"Role {role.mention} assigned", color=0x23cf4b,timestamp=datetime.datetime.utcnow())
-                            if str(config['webhook']['custom-image']).startswith("https") or str(config['webhook']['custom-image']).startswith("http"):
+                            embed = discord.Embed(title=f"{member} joined", description=f"Role {role.mention} assigned", color=0x23cf4b,timestamp=datetime.datetime.utcnow()) #You can edit this embed message
 
-                                await webhook.send(embed=embed, username=config['webhook']['custom-username'], avatar_url=config['webhook']['custom-image'])
-                            else:
-                                await webhook.send(embed=embed, username=config['webhook']['custom-username'])
-
-
-
-                    except Exception as e:
-                        print(e)
-        except TypeError:
-            role = member.guild.get_role(config['roles']['user-roles'])
-            await member.add_roles(role, reason="Autorole")
-
-            if config['webhook']['enabled']:
-                try:
-                    webhook_url = str(config['webhook']['url'])
-
-
-                    async with aiohttp.ClientSession() as session:
-
-                        webhook = Webhook.from_url(webhook_url , adapter=AsyncWebhookAdapter(session))
-
-                        embed = discord.Embed(title=f"{member} joined", description=f"Role {role.mention} assigned", color=0x23cf4b,timestamp=datetime.datetime.utcnow())
-                        if str(config['webhook']['custom-image']).startswith("https") or str(config['webhook']['custom-image']).startswith("http"):
-
+                        try:
                             await webhook.send(embed=embed, username=config['webhook']['custom-username'], avatar_url=config['webhook']['custom-image'])
-                        else:
+                        except HTTPException:
+                            print("Specify a valid image url on the config file")
                             await webhook.send(embed=embed, username=config['webhook']['custom-username'])
-
-
-
-                except Exception as e:
-                    print(e)
+                elif role == None:
+                    raise TypeError("You have to give me a valid id")
+            
 
 
 @bot.event
-async def on_command_error(ctx, error):
+async def on_command_error(ctx, error): #Ignore errors on commands because we dont have any commands
     pass
 
-bot.run(config['bot']['token'])
+bot.run(config['bot']['token']) #Just run the bot with the given token on config.json
